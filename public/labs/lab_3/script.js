847f1fa32f146a2017ffd26f03b8b9a28b70d8ca
augustmeyer <75350319+augustmeyer@users.noreply.github.com>
2021-02-17 15:57:45 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,18 @@
-/* Put your javascript in here */
+const button = document.createElement('button');
+
+function moveNext() {
+  button.addEventListener('click', (event) => {
+    const array1 = [images];
+    const listContainer = document.createElement('ul');
+    const target = document.querySelector('.next');
+    target.append(listContainer);
+    
+    const array2 = array1.map(element => {
+      const listItem = document.createElement('li');
+      listContainer.append(listItem);
+      return typeof element;
+    }
+    )
+})
+}
+window.onload = moveNext();
